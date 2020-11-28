a7ed2040995fa54e99621615f1546609281904e7
defunsm <salmanhossain500@gmail.com>
2020-11-28 17:51:44 -0500

    new func to help convert move selection to int for boardState

--- Board.cpp
+++ Board.cpp
@@ -4,6 +4,9 @@
 //#include "Board.h" removing since repl has problems with .h files
 using namespace std;
 
+
+// enum Move { "1 1", "1 2", "1 3", "2 1", "2 2", "2 3", "3 1", "3 2", "3 3" };
+
 class Board
 {
 
@@ -34,27 +37,42 @@ class Board
     bool checkForWin(); // checking for win returns false if no player won true if someone won
     string getTimeStamp(); // returns a time as a string
     void logMoves(); // log moves onto a log.txt file
+
+    int convertMove(string move) {
+        if (move == "1 1") return 0;
+        else if (move == "1 2") return 1;
+        else if (move == "1 3") return 2;
+        else if (move == "2 1") return 3;
+        else if (move == "2 2") return 4;
+        else if (move == "2 3") return 5;
+        else if (move == "3 1") return 6;
+        else if (move == "3 2") return 7;
+        else if (move == "3 3") return 8;
+        else { return -1; }
+    }
+
+
     void makeMove(string move) // allows player to make a move
     {
         cout << endl;
 
-        int moveSelected = -1;  // not valid move but place holder
+        // redo so that it sets selected move to an index so that it can be used with varify
+        int playerMove = convertMove(move);
 
-        if (move == "1 1") { this->boardState[0]=this->currentPlayerTurn; }
-        else if (move == "1 2") { this->boardState[1]=this->currentPlayerTurn; }
-        else if (move == "1 3") { this->boardState[2]=this->currentPlayerTurn; }
-        else if (move == "2 1") { this->boardState[3]=this->currentPlayerTurn; }
-        else if (move == "2 2") { this->boardState[4]=this->currentPlayerTurn; }
-        else if (move == "2 3") { this->boardState[5]=this->currentPlayerTurn; }
-        else if (move == "3 1") { this->boardState[6]=this->currentPlayerTurn; }
-        else if (move == "3 2") { this->boardState[7]=this->currentPlayerTurn; }
-        else if (move == "3 3") { this->boardState[8]=this->currentPlayerTurn; }
-        else {}
+        if (playerMove == -1) {
+            throw("Invalid Move");
+        } else {
+            this->boardState[playerMove] = this->currentPlayerTurn;
+        }
+        
 
         if (this->currentPlayerTurn=='X') { this->currentPlayerTurn='O'; }
         else { this->currentPlayerTurn='X'; }
     }
-    void checkValidMove(); // checks that the move made was valid
+    bool checkValidMove() // checks that the move made was valid
+    {
+
+    }
 
     private:
 
