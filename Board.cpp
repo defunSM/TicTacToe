6f607903b9ed37cdbf11219fde4fcb83c1e02dfd
defunsm <salmanhossain500@gmail.com>
2020-11-27 23:31:15 -0500

    adding functions to a class called Board

--- Board.cpp
+++ Board.cpp
@@ -8,18 +8,55 @@ class Board
 {
 
   public:
-    Board(string player1, string player2, int* boardstate);
-    void showMenu(); // displays menu
-    void displayBoard(int *boardState); // create the tictactoe board from boardState array
-    void checkForWin(); // checking for win
+    Board()
+    {
+        this->boardState = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        this->currentPlayerTurn = 'X';
+    }
+
+    void showMenu() // displays menu
+    {
+        cout << "----------------------------" << endl;
+        cout << "         Tic Tac Toe        " << endl;
+        cout << "----------------------------" << endl;
+    }
+
+    void displayBoard() // create the tictactoe board from boardState array
+    {
+
+        cout << "      1   2   3  " << endl;
+        cout << "   1 [" << this->boardState[0] << "] [" << this->boardState[1] << "] [" << this->boardState[2] << "] " << endl;
+        cout << "   2 [" << this->boardState[3] << "] [" << this->boardState[4] << "] [" << this->boardState[5] << "] " << endl;
+        cout << "   3 [" << this->boardState[6] << "] [" << this->boardState[7] << "] [" << this->boardState[8] << "] " << endl;
+        cout << endl;
+
+    }
+    bool checkForWin(); // checking for win returns false if no player won true if someone won
     string getTimeStamp(); // returns a time as a string
     void logMoves(); // log moves onto a log.txt file
-    void makeMove(string move); // allows player to make a move
+    void makeMove(string move) // allows player to make a move
+    {
+        cout << endl;
+
+        int moveSelected = -1;  // not valid move but place holder
+
+        if (move == "1 1") { this->boardState[0]=this->currentPlayerTurn; }
+        else if (move == "1 2") { this->boardState[1]=this->currentPlayerTurn; }
+        else if (move == "1 3") { this->boardState[2]=this->currentPlayerTurn; }
+        else if (move == "2 1") { this->boardState[3]=this->currentPlayerTurn; }
+        else if (move == "2 2") { this->boardState[4]=this->currentPlayerTurn; }
+        else if (move == "2 3") { this->boardState[5]=this->currentPlayerTurn; }
+        else if (move == "3 1") { this->boardState[6]=this->currentPlayerTurn; }
+        else if (move == "3 2") { this->boardState[7]=this->currentPlayerTurn; }
+        else if (move == "3 3") { this->boardState[8]=this->currentPlayerTurn; }
+        else {}
+    }
     void checkValidMove(); // checks that the move made was valid
 
-  private:
-    int* boardState; // current state of the tictactoe board
-    vector<int> boardLogs; // logs of the board
+    private:
 
+        vector<char> boardState; // current state of the tictactoe board
+        vector<string> boardLogs;   // logs of the board
+        char currentPlayerTurn;  // the player turn
 };
 
--- main.cpp
+++ main.cpp
@@ -1,87 +1,37 @@
 #include <iostream>
 #include <string>
+#include "Board.cpp"
 
 #define Prompt() cout << "> " // defining the prompt '> ' for asking player
 
 using namespace std;
 
-void showMenu(); // displays menu
-void displayBoard(char* boardState);		// create the tictactoe board from boardState array
-void checkForWin();			// checking for win
-string getTimeStamp();		// returns a time as a string
-void logMoves();			// log moves onto a log.txt file
-void makeMove(string move, char* boardState, char currentPlayerTurn); // allows player to make a move
-void checkValidMove();		// checks that the move made was valid
 
 int main() {
-	string option;
+	string userInput;
 	string playerOneName, playerTwoName;
 	char boardState[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}; // variable representing the tictactoe board
 	int settings[2] = {0, 0};		// player, win condition
 
-	showMenu();
+  Board *b = new Board;  // Initializing new object board
+
+
+	b->showMenu();
 	cout << "Select by entering '1' or '2'" << endl << endl;
 	cout << "1) Player vs Player " << endl;
 	cout << "2) Player vs Computer " << endl << endl;
-	Prompt(); 
-  getline(cin, option);
+	Prompt();
+  getline(cin, userInput);
 	cout << endl;
 
-	while (settings[1] == 0) {
-		displayBoard(boardState);
-		cout << "Enter your next move! (i.e '1 2' means row 1 column 2)" << endl << endl;
-		Prompt(); 
-    getline(cin, option);
-		makeMove(option, boardState, 'X');
-	}
-}
+  while (true) {
 
-void makeMove(string move, char* boardState, char currentPlayerTurn) {
-	cout << endl;
-
-  int moveSelected = -1;  // not valid move but place holder
-
-  if (move == "1 1") { moveSelected=0; }
-  else if (move == "1 2") { moveSelected=1; }
-  else if (move == "1 3") { moveSelected=2; }
-  else if (move == "2 1") { moveSelected=3; }
-  else if (move == "2 2") { moveSelected=4; }
-  else if (move == "2 3") { moveSelected=5; }
-  else if (move == "3 1") { moveSelected=6; }
-  else if (move == "3 2") { moveSelected=7; }
-  else if (move == "3 3") { moveSelected=8; }
-  else 
-  { 
-    string move;
-    displayBoard(boardState);
-    cout << "You did not enter a valid move! Try again." << endl << endl;
+    b->displayBoard();
     cout << "Enter your next move! (i.e '1 2' means row 1 column 2)" << endl << endl;
-		Prompt(); 
-    getline(cin, move);
-    makeMove(move, boardState, currentPlayerTurn);
-  }
-  if (currentPlayerTurn == 'X') 
-  {
-    boardState[moveSelected] = 'X';
-  } else {
-    boardState[moveSelected] = 'O';
+    Prompt();
+    getline(cin, userInput);
+    b->makeMove(userInput);
+
   }
 
-  currentPlayerTurn = 'O';
-
-}
-
-void showMenu() {
-	cout << "----------------------------" << endl;
-	cout << "         Tic Tac Toe        " << endl;
-	cout << "----------------------------" << endl;
-}
-
-void displayBoard(char* boardState) {
-
-	cout << "      1   2   3  " << endl;
-	cout << "   1 [" << boardState[0] << "] [" << boardState[1] << "] [" << boardState[2] << "] " << endl;
-	cout << "   2 [" << boardState[3] << "] [" << boardState[4] << "] [" << boardState[5] << "] " << endl;
-	cout << "   3 [" << boardState[6] << "] [" << boardState[7] << "] [" << boardState[8] << "] " << endl;
-  cout << endl;
 }
