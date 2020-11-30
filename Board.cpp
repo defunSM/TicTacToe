552199aab410c7af6d5f0b2f1afbfb37f6b965bf
defunsm <salmanhossain500@gmail.com>
2020-11-30 00:40:34 -0500

    checkValidMove and redos if invalid move

--- Board.cpp
+++ Board.cpp
@@ -4,6 +4,7 @@
 //#include "Board.h" removing since repl has problems with .h files
 using namespace std;
 
+#define Prompt() cout << "> " // defining the prompt '> ' for asking player
 
 // enum Move { "1 1", "1 2", "1 3", "2 1", "2 2", "2 3", "3 1", "3 2", "3 3" };
 
@@ -51,6 +52,11 @@ class Board
         else { return -1; }
     }
 
+    bool checkValidMove(int playerMove) // checks that the move made was valid
+    {
+        if(this->boardState[playerMove]!=' ') { return false; }
+        else { return true; }
+    }
 
     void makeMove(string move) // allows player to make a move
     {
@@ -58,21 +64,21 @@ class Board
 
         // redo so that it sets selected move to an index so that it can be used with varify
         int playerMove = convertMove(move);
+        string newMove; // incase invalid Move is selected
 
         if (playerMove == -1) {
-            throw("Invalid Move");
-        } else {
-            this->boardState[playerMove] = this->currentPlayerTurn;
+            cout << "Invalid Move!" << endl;
+            Prompt();
+            getline(cin, newMove);
+            makeMove(newMove);
         }
+        else { this->boardState[playerMove] = this->currentPlayerTurn; }
         
-
+        // Changes player turn
         if (this->currentPlayerTurn=='X') { this->currentPlayerTurn='O'; }
         else { this->currentPlayerTurn='X'; }
     }
-    bool checkValidMove() // checks that the move made was valid
-    {
 
-    }
 
     private:
 
